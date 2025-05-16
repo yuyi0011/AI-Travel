8ebd3e6e41726fb0a062e96348cf806f727bbc7d
yuyi0011 <yuyi0011@stud.kea.dk>
2025-05-16 09:04:53 +0200

    feat: Add initial setup for AI travel application
    - Introduced new components for Header, Hero, and CreateTrip.
    - Implemented UI elements including buttons and inputs with Tailwind CSS.
    - Integrated Google Places API for destination selection.
    - Added AI functionality for generating travel plans using Google Generative AI.
    - Created constants for travel options and prompts.
    - Established routing with React Router for navigation between main app and trip creation.
    - Included utility functions for class name management and toast notifications.
    - Set up Tailwind CSS and PostCSS for styling.
    - Added SVG assets for branding and icons.
    - Implemented responsive design for better user experience.

---
+++ AI-travel/src/service/firebaseConfig.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPgXvbL-yo5VZPmQva5hZxdk192uJ02cI",
+  authDomain: "yz-project-1ba60.firebaseapp.com",
+  databaseURL: "https://yz-project-1ba60-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "yz-project-1ba60",
+  storageBucket: "yz-project-1ba60.appspot.com",
+  messagingSenderId: "639129222976",
+  appId: "1:639129222976:web:5abc83c241d0a87d0c04a5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
